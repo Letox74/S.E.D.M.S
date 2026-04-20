8e77e168b88bee74f4ac37d327be0682d3d03c32
Letox74 <letox0828@gmail.com>
2026-04-20 16:48:45 +0200

    feat: logic to generate new api key (also can override the env)

--- internal/security.py
+++ internal/security.py
@@ -1,6 +1,9 @@
+import secrets
+
+from dotenv import load_dotenv, set_key
 from fastapi import HTTPException, status
 
-from core.config import API_KEY
+from core.config import API_KEY, ENV_PATH
 
 
 async def verify_api_key(provided_api_key: str) -> None:
@@ -9,3 +12,12 @@ async def verify_api_key(provided_api_key: str) -> None:
             status_code=status.HTTP_403_FORBIDDEN,
             detail="The provided API-Key does not match with the saved one"
         )
+
+def generate_new_api_key(nbytes: int = 32, override_in_env: bool = True):
+    new_key = secrets.token_hex(nbytes)
+
+    if override_in_env:
+        set_key(ENV_PATH, "API_KEY", new_key)
+        load_dotenv(override=True)
+
+    return new_key
