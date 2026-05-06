278d671b61a1b3fbbfa0bb4f6588c733913dde26
Letox74 <letox0828@gmail.com>
2026-05-06 19:52:26 +0200

    refactor: shift few values back to the config from the env

--- core/config.py
+++ core/config.py
@@ -20,36 +20,37 @@ API_KEY: str = os.getenv("API_KEY")
 
 # URLs
 BASE_URL: str = os.getenv("BASE_URL")
-PORT: int = int(os.getenv("PORT"))
+API_PORT: int = int(os.getenv("API_PORT"))
+STREAMLIT_PORT: int = int(os.getenv("STREAMLIT_PORT"))
 
-PREFIX: str = os.getenv("PREFIX")
-DOCS_URL: Optional[str] = PREFIX + os.getenv("DOCS_URL")
-REDOC_URL: Optional[str] = PREFIX + os.getenv("REDOC_URL")
-OPENAPI_URL: Optional[str] = PREFIX + os.getenv("OPENAPI_URL")
+PREFIX: str = "/sedms/api"
+DOCS_URL: Optional[str] = PREFIX + "/docs"
+REDOC_URL: Optional[str] = PREFIX + "/redoc"
+OPENAPI_URL: Optional[str] = PREFIX + "/openapi.json"
 
 # CORS
-USE_CORS: bool = os.getenv("USE_CORS").lower().strip() in ("true", "1", "yes")
-ALLOW_CREDENTIALS: bool = os.getenv("ALLOW_CREDENTIALS").lower().strip() in ("true", "1", "yes")
-ALLOWED_ORIGINS: list[str] | list[Never] = list(os.getenv("ALLOWED_ORIGINS"))
-ALLOWED_METHODS: list[str] | list[Never] = list(os.getenv("ALLOWED_METHODS"))
-ALLOWED_HEADERS: list[str] | list[Never] = list(os.getenv("ALLOWED_HEADERS"))
+USE_CORS: bool = False
+ALLOW_CREDENTIALS: bool = True
+ALLOWED_ORIGINS: list[str] | list[Never] = ["*"]
+ALLOWED_METHODS: list[str] | list[Never] = ["*"]
+ALLOWED_HEADERS: list[str] | list[Never] = ["*"]
 
 # rate limits
 DEFAULT_RATE_LIMIT: str = os.getenv("DEFAULT_RATE_LIMIT")
-ACTIVATE_RATE_LIMITS: bool = os.getenv("ACTIVATE_RATE_LIMITS").lower().strip() in ("true", "1", "yes")
+ACTIVATE_RATE_LIMITS: bool = True
 
 # Telemetry
-TELEMETRY_LIMIT: int = int(os.getenv("TELEMETRY_LIMIT"))
+TELEMETRY_LIMIT: int = 1 # in minutes
 
 # ml stuff (soon, just examples)
 RETRAIN_SCHEDULER: Optional[str] = None
 
 # four models in total
-PREDICTION_HORIZONS: list[int] = list(map(lambda x: int(x), os.getenv("PREDICTION_HORIZONS").split(", "))) # in minutes
+PREDICTION_HORIZONS: list[int] = [15, 60, 360, 1440] # in minutes
 # can be refactored later, that the user decides the three models
 
 # other stuff
-IGNORE_WARNINGS: bool = os.getenv("IGNORE_WARNINGS").lower().strip() in ("true", "1", "yes")
+IGNORE_WARNINGS: bool = False
 
 # frontend
 FRONTEND_PASSWORD: str = os.getenv("FRONTEND_PASSWORD")
